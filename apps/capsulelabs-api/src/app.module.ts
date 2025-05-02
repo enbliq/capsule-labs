28c22132df991edd84af44568e4688ec0ac6107b
Joy <alisjoy1997@gmail.com>
2025-05-02 12:08:55 +0100

    migrated to nestjs and added terminus

---
+++ apps/capsulelabs-api/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { HealthModule } from './health/health.module';
+
+@Module({
+  imports: [
+    HealthModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+        connectionFactory: (connection) => {
+          console.log('Database connected:', connection.name);
+          return connection;
+        },
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
--- apps/capsulelabs-api/src/routes/health.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const healthRouter = (router: express.Router) => {
-  router.get('/health', (req, res) => {
-    res.status(200).json({ greeting: 'Hello World!' });
-  });
-};
-
-export default healthRouter;
