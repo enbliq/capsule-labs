dd944dd38dbf01db31500f09be4eb07d0cfabccc
Joy Ali <109388815+inteee@users.noreply.github.com>
2025-05-03 02:33:13 +0100

    Update app.module.ts

--- apps/capsulelabs-api/src/app.module.ts
+++ apps/capsulelabs-api/src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { HealthModule } from './health/health.module';
+import { TimeBombCapsuleModule } from "./capsules/timebomb/timebomb-capsule.module"
 
 @Module({
   imports: [
+    TimeBombCapsuleModule,
     HealthModule,
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRootAsync({
